a2c996334a454212b48bd4da404f87eeb5a6847f
wb-sml406419 <wb-sml406419@alibaba-inc.com>
2018-09-21 15:38:05 +0800

    feat: 修改log 配置

---
+++ app/utils/constants.js
@@ -0,0 +1,10 @@
+const log_level = {
+  TRACE: '[TRACE]',
+  DEBUG: '[DEBUG]',
+  INFO: '[INFO]',
+  WARN: '[WARN]',
+  ERROR: '[ERROR]',
+  FATAL: '[FATAL]',
+};
+
+export default log_level;
--- config/config.default.js
+++ config/config.default.js
@@ -2,43 +2,52 @@
 
 const path = require('path');
 
-module.exports = {
-  keys: 'secret',
-  // rest: {
-  //   urlprefix: '/rest/api/',
-  //   authRequest: null,
-  //   authIgnores: null,
-  // },
-  router: {
-    root: '/pages',
-    entry: 'page',
-  },
-  middleware: [
-    // 'jsonp',
-    'gzip',
-  ],
-  gzip: {
-    threshold: 1024, // 小于 1k 的响应体不压缩
-  },
-  react: {
-    static: true,
-    // middlewares: [
-    //   'time',
-    //   'cache',
-    //   'redux',
-    //   'partial',
-    //   'render',
-    //   'doctype',
-    //   'beautify',
-    // ],
-  },
-  security: {
-    domainWhiteList: ['localhost:6001'],
-  },
-  session: {
-    key: 'BEIDOU_SESS',
-    maxAge: 1 * 3600 * 1000, // 1 hour
-    httpOnly: true,
-    encrypt: true,
-  },
+module.exports = (appInfo) => {
+  return {
+    keys: 'secret',
+    // rest: {
+    //   urlprefix: '/rest/api/',
+    //   authRequest: null,
+    //   authIgnores: null,
+    // },
+    router: {
+      root: '/pages',
+      entry: 'page',
+    },
+    middleware: [
+      // 'jsonp',
+      'gzip',
+    ],
+    gzip: {
+      threshold: 1024, // 小于 1k 的响应体不压缩
+    },
+    react: {
+      static: true,
+      // middlewares: [
+      //   'time',
+      //   'cache',
+      //   'redux',
+      //   'partial',
+      //   'render',
+      //   'doctype',
+      //   'beautify',
+      // ],
+    },
+    security: {
+      domainWhiteList: ['localhost:6001'],
+    },
+    session: {
+      key: 'BEIDOU_SESS',
+      maxAge: 1 * 3600 * 1000, // 1 hour
+      httpOnly: true,
+      encrypt: true,
+    },
+    logger: {
+      dir: path.resolve(__dirname, `../logs/${appInfo.name}`),
+      appLogName: `${appInfo.name}-web.log`,
+      coreLogName: 'egg-web.log',
+      agentLogName: 'egg-agent.log',
+      errorLogName: 'common-error.log',
+    },
+  };
 };
